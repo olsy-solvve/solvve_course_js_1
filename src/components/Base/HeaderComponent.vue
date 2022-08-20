8d5b2274b7211c7be30baa4dac95303f41e4a9ce
Master <markov.denis.1999@gmail.com>
2022-08-20 18:45:56 +0300

    Add header with adaptation

--- src/components/Base/HeaderComponent.vue
+++ src/components/Base/HeaderComponent.vue
@@ -11,27 +11,28 @@ export default {
       routes: [
         {
           label: "HOME",
-          icon: "pi pi-fw pi-home",
+          icon: "pi  pi-home",
           to: { name: routesName.HOME },
         },
         {
           label: "TODO",
-          icon: "pi pi-plus",
+          icon: "pi pi-calendar-plus",
           to: { name: routesName.TODO },
         },
         {
           label: "ARCHIVE",
-          icon: "pi pi-minus",
+          icon: "pi pi-database",
           to: { name: routesName.ARCHIVE },
         },
         {
           label: "DOC",
-          icon: "pi pi-fw pi-file",
+          icon: "pi pi-file",
           to: { name: routesName.DOC },
         },
       ],
       userConfirmation: false,
       logoImage: "@/assets/logoTodo.svg",
+      headerNavigation: "@/assets/headerNavigation.svg",
     };
   },
   methods: {},
@@ -43,26 +44,26 @@ export default {
 };
 </script>
 
+<!-- https://coreui.io/vue/docs/layout/breakpoints.html -->
+<!-- sm: small screens e.g. phones
+md: medium screens e.g. tablets
+lg: large screens e.g. notebooks
+xl: larger screens e.g monitors -->
+
 <template>
-  <PrimeMenubar :model="routes">
-    <template #start>
-      <div class="menubar-start">
-        <div>
-          <img
-            alt="logo"
-            src="@/assets/logoTodo.svg"
-            class="menubar-logo-image"
-          />
-        </div>
-        <div class="menubar-logo-text">SUPER PUPER</div>
+  <header class="sticky w-full top-0 left-0 z-5 bg-bluegray-100">
+    <div class="flex flex-row p-2">
+      <div
+        class="flex flex-1 flex-row justify-content-start align-items-center"
+      >
+        <img alt="logo" src="@/assets/logoTodo.svg" class="header-logo-image" />
+        <p class="ml-3 text-red-700 header-logo-text">SUPER PUPER</p>
       </div>
-    </template>
-    <template #end>
-      <div class="menubar-end">
-        <div class="col-12 md:col-4">
+      <div class="flex flex-1 flex-row justify-content-end align-items-center">
+        <div class="col-8 hidden md:inline-flex">
           <div class="p-inputgroup">
             <PrimeInputText placeholder="Keyword" />
-            <BaseButton icon="pi pi-search" class="p-button-warning" />
+            <BaseButton icon="pi pi-search" class="p-button-success" />
           </div>
         </div>
         <div v-if="userConfirmation">
@@ -80,34 +81,57 @@ export default {
           />
         </div>
       </div>
-    </template>
-  </PrimeMenubar>
+    </div>
+    <div>
+      <PrimeMenubar :model="routes">
+        <template #start>
+          <img
+            alt="logo"
+            src="@/assets/headerNavigation.svg"
+            class="header-navigation-image"
+          />
+        </template>
+      </PrimeMenubar>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
-.p-menubar {
-  background-color: beige;
+.header-logo-image {
+  height: 60px;
+}
+.header-logo-text {
+  font-family: $app-text-logo;
+  font-size: 36px;
+}
 
-  & .menubar-start {
-    @include menubar-start-end;
+.header-navigation-image {
+  height: 40px;
+}
 
-    & .menubar-logo-image {
-      height: 60px;
-    }
-
-    & .menubar-logo-text {
-      font-family: $app-text-logo;
-      font-size: 32px;
-      color: brown;
-    }
+@media (max-width: 768px) {
+  .header-logo-image {
+    height: 50px;
   }
-
-  & .p-menubar-root-list {
-    @include menubar-start-end;
+  .header-logo-text {
+    font-family: $app-text-logo;
+    font-size: 32px;
   }
+  .header-navigation-image {
+    height: 35px;
+  }
+}
 
-  & .menubar-end {
-    @include menubar-start-end;
+@media (max-width: 576px) {
+  .header-logo-image {
+    height: 40px;
+  }
+  .header-logo-text {
+    font-family: $app-text-logo;
+    font-size: 24px;
+  }
+  .header-navigation-image {
+    height: 30px;
   }
 }
 </style>
