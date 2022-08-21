3079b96c8578c989f1a205e62011869edc8217fe
keysiu <43180469+keysiu@users.noreply.github.com>
2022-08-21 23:05:10 +0300

    added list-navigation logics

--- src/features/TodoListFeature.vue
+++ src/features/TodoListFeature.vue
@@ -1,9 +1,10 @@
 <script>
 import PrimeTabMenu from "primevue/tabmenu";
 import "primeicons/primeicons.css";
-import PrimePanelMenu from "primevue/panelmenu";
+import PrimeListBox from "primevue/listbox";
 import PrimeCard from "primevue/card";
 import PrimeButton from "primevue/button";
+import store from "@/store/index.js";
 
 let filters = [
   { label: "All", icon: "pi pi-th-large" },
@@ -12,49 +13,15 @@ let filters = [
   { label: "Delete list" },
 ];
 
+
 const addTodoCard = [{ label: "ADD NEW TODO", discription: "" }];
 
-const addTodoList = [{ label: "", icon: "pi pi-plus" }];
+const addTodoList = [{ label: "ADD NEW TODO LIST", icon: "pi pi-plus" }];
 
-let todoList = [
-  {
-    label: "Home things",
-    list: [
-      {
-        label: "Clean room",
-        disription: "And faster!",
-      },
-      {
-        label: "Wash dishes",
-        discription: null,
-      },
-    ],
-  },
-  {
-    label: "Work",
-    list: [{ label: "Progect" }, { label: "Call to Sam" }],
-  },
-  {
-    label: "Univercity",
-    list: [{ label: "End lection" }, { label: "Create design" }],
-  },
-  {
-    label: "Family",
-    list: [{ label: "Call to mam" }, { label: "Buy a doll for Kristy" }],
-  },
-  {
-    label: "Anime list",
-    list: [
-      {
-        label: "Death Note",
-        discription:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deseruntquisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!",
-      },
-      { label: "Fairy Tale", discription: "3rd season is out!" },
-      { label: "Naruto", discription: "Let`s just try..." },
-    ],
-  },
-];
+let todoList;
+let listLength;
+
+let currentList = "Anime list";
 
 export default {
   data() {
@@ -63,114 +30,105 @@ export default {
       todoList,
       addTodoList,
       addTodoCard,
+      currentList,
+      listLength
     };
   },
   components: {
     PrimeTabMenu,
-    PrimePanelMenu,
+    PrimeListBox,
     PrimeCard,
     PrimeButton,
   },
+  mounted() {
+    this.todoList = this.$store.getters["todoStore/getTodos"];
+    this.listLength = this.$store.getters["todoStore/getListLength"];
+  },
+  computed: {
+  }
 };
 </script>
 
 <template>
   <div>
-    <div class="under-panel-menu"></div>
-    <div>
-      <PrimePanelMenu :model="addTodoList" class="add-todo-list">
-      </PrimePanelMenu>
-      <PrimePanelMenu :model="todoList" class="panel-menu"> </PrimePanelMenu>
-    </div>
 
-    <div class="filter-todos-position">
-      <PrimeTabMenu :model="filters" class="filter-todos" />
-      <router-view />
-    </div>
+    <div class="flex flex-row">
+      <div>
+        <PrimeListBox :options="addTodoList" optionLabel="label">
+        </PrimeListBox>
+        <PrimeListBox :options="todoList" optionLabel="label">
+          <template #option="slotProps">
+            <div @click="currentList = slotProps.option.label">{{ slotProps.option.label }}</div>
+          </template>
+        </PrimeListBox>
+      </div>
+      <div class="flex flex-column">
+        <div>
+          <PrimeTabMenu :model="filters" class="flex-wrap mt-2" />
+        </div>
+        <ul class="todo-card ml-2 mt-2 flex-wrap">
+          <li>
+            <PrimeCard style="width: 25rem; margin-bottom: 2em">
+              <template #title>
+                {{ addTodoCard[0].label }}
+              </template>
+            </PrimeCard>
 
-    <ul class="todo-card">
-      <li>
-        <PrimeCard style="width: 25rem; margin-bottom: 2em">
-          <template #title>
-            {{ addTodoCard[0].label }}
-          </template>
-        </PrimeCard>
-      </li>
-      <li>
-        <PrimeCard
-          v-for="labels in todoList[todoList.length - 1].list"
-          :key="labels.label"
-          style="width: 25rem; margin-bottom: 2em"
-        >
-          <template #title>
-            {{ labels.label }}
-          </template>
-          <template #content>
-            <p>{{ labels.discription }}</p>
-            <div class="edit-button">
-              <PrimeButton icon="pi pi-pencil" class="p-button-rounded" />
+
+          </li>
+          <div v-for="labels in todoList">
+            <div v-if="labels.label === currentList">
+              <div v-for="cards in labels.list">
+
+              </div>
             </div>
-            <div class="delete-button">
-              <PrimeButton
-                icon="pi pi-times"
-                class="p-button-rounded p-button-danger"
-              ></PrimeButton>
+          </div>
+          <li>
+            <div v-for="labels in todoList">
+              <div v-if="labels.label === currentList">
+                <PrimeCard v-for="cards in labels.list" :key="cards.label" style="width: 25rem; margin-bottom: 2em">
+                  <template #title>
+                    {{ cards.label }}
+                  </template>
+
+
+                  <template #content>
+                    <p>{{ cards.discription }}</p>
+                    <div class="edit-button">
+                      <PrimeButton icon="pi pi-pencil" class="p-button-rounded" />
+                    </div>
+                    <div class="delete-button">
+                      <PrimeButton icon="pi pi-times" class="p-button-rounded p-button-danger"></PrimeButton>
+                    </div>
+                  </template>
+                </PrimeCard>
+              </div>
             </div>
-          </template>
-        </PrimeCard>
-      </li>
-    </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
 .edit-button {
   padding-bottom: 8px;
   padding-left: 320px;
 }
 
-.under-panel-menu {
-  height: 100%;
-  position: absolute;
-  width: 20em;
-  max-width: 100%;
-  top: 12.2em;
-  background-color: rgba(255, 255, 255, 0.367);
-}
 
-.add-todo-list {
-  width: 20em;
-  max-width: 100%;
-  min-height: 2em;
-  position: absolute;
-}
 
-.panel-menu {
-  width: 20em;
-  max-width: 100%;
-  min-height: 2em;
-  position: absolute;
-  top: 12.2em;
-}
-
-.filter-todos {
-  width: 200em;
-  max-width: 100%;
-  min-height: 2em;
-}
 
 .todo-card {
   list-style-type: none;
-  padding-left: 400px;
 }
 
 body {
-  background-color: #eff3f8;
+  background-color: rgba(255, 255, 255, 0.235);
 }
 
-.filter-todos-position {
-  padding-left: 320px;
-}
+
 
 .delete-button {
   padding-left: 320px;
