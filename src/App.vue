07fc13120e1541218dd489b2fc40ded3c9e1f1b0
Master <markov.denis.1999@gmail.com>
2022-08-23 10:18:03 +0300

    Modified HeaderComponent, HomeComponent, ErrorComponent

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
 import HeaderComponent from "@/components/Base/HeaderComponent.vue";
 import MainComponent from "@/components/Base/MainComponent.vue";
 import FooterComponent from "@/components/Base/FooterComponent.vue";
-import TodoPopup from "@/components/UI/TodoPopup.vue";
+import TodoCreatePopup from "@/components/UI/TodoCreatePopup.vue";
 
 export default {
   components: {
     HeaderComponent,
     MainComponent,
     FooterComponent,
-    TodoPopup,
+    TodoCreatePopup,
   },
   computed: {
     getPopups() {
--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,10 @@ import { createStore } from "vuex";
 import todoStore from "@/store/todo/todoStore.js";
 import userStore from "@/store/user/userStore.js";
 import popupStore from "@/store/popup/popupStore.js";
+import routerStore from "@/store/router/routerStore.js";
 
 const store = createStore({
-  modules: { todoStore, userStore, popupStore },
+  modules: { todoStore, userStore, popupStore, routerStore },
 });
 
 export default store;
