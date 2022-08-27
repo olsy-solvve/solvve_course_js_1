a2b224298c23e83cc2b7a0f4532dc5da07c68ffe
keysiu <43180469+keysiu@users.noreply.github.com>
2022-08-28 01:03:38 +0300

    added archive

--- src/components/Todo/TodoItem.vue
+++ src/components/Todo/TodoItem.vue
@@ -29,9 +29,9 @@ export default {
     },
     showNewType() {
       this.$toast.add({
-        severity: "success",
-        summary: "Success Message",
-        detail: "Order submitted",
+        severity: "info",
+        summary: "HEY!",
+        detail: "You changed your TODO`s completing! Be careful!",
         life: 3000,
       });
     },
@@ -40,12 +40,9 @@ export default {
 </script>
 
 <template>
-  <PrimeToast position="bottom-right" />
-  <PrimeCard
-    @click="changeFilter(todo.label), showNewType()"
-    :class="{ isDone: todo.completed }"
-    style="width: 25rem; margin-bottom: 2em"
-  >
+  <PrimeToast position="bottom-right" class="opacityToast" />
+  <PrimeCard @click="changeFilter(todo.label), showNewType()" :class="{ isDone: todo.completed }"
+    style="width: 25rem; margin-bottom: 2em">
     <template #title>
       <h4 class="text-center">
         {{ todo.label }}
@@ -54,19 +51,16 @@ export default {
     <template #content>
       <p>{{ todo.discription }}</p>
       <div class="flex justify-content-end pt-3">
-        <BaseButton
-          icon="pi pi-pencil"
-          class="p-button-rounded"
-          @click.stop="openTodoEditPopup"
-        />
-        <BaseButton
-          icon="pi pi-times"
-          class="p-button-rounded p-button-danger ml-2"
-          @click.stop="removeTodo(todo.id)"
-        />
+        <BaseButton icon="pi pi-pencil" class="p-button-rounded" @click.stop="openTodoEditPopup" />
+        <BaseButton icon="pi pi-times" class="p-button-rounded p-button-danger ml-2"
+          @click.stop="removeTodo(todo.id)" />
       </div>
     </template>
   </PrimeCard>
 </template>
 
-<style></style>
+<style>
+.opacityToast {
+  opacity: 0.5;
+}
+</style>
--- src/store/stores/todoStore.js
+++ src/store/stores/todoStore.js
@@ -4,15 +4,19 @@ const state = {
   todos: [
     {
       label: "Home things",
+      removed: true,
+      fullRemoved: false,
       list: [
         {
           id: 1,
+          removed: true,
           label: "Clean room",
           completed: false,
           discription: "And faster!",
         },
         {
           id: 2,
+          removed: false,
           label: "Wash dishes",
           completed: true,
           discription: null,
@@ -21,32 +25,45 @@ const state = {
     },
     {
       label: "Work",
+      removed: false,
+      fullRemoved: false,
       list: [
-        { id: 1, label: "Progect", completed: true },
-        { id: 2, label: "Call to Sam", completed: false },
+        {
+          id: 1, label: "Progect",
+          removed: false,
+          completed: true
+        },
+        { id: 2, removed: false, label: "Call to Sam", completed: false },
       ],
     },
     {
       label: "Study",
+      removed: false,
+      fullRemoved: false,
       list: [
-        { id: 1, label: "End lection", completed: false },
-        { id: 2, label: "Create design", completed: true },
+        { id: 1, removed: false, label: "End lection", completed: false },
+        { id: 2, removed: false, label: "Create design", completed: true },
       ],
     },
     {
       label: "Family",
+      removed: false,
+      fullRemoved: false,
       list: [
-        { id: 1, label: "Call to mam", completed: true },
-        { id: 2, label: "Buy a doll for Kristy", completed: true },
+        { id: 1, removed: false, label: "Call to mam", completed: true },
+        { id: 2, removed: false, label: "Buy a doll for Kristy", completed: true },
       ],
     },
     {
       label: "Anime list",
+      removed: true,
+      fullRemoved: false,
       list: [
         {
           id: 1,
           label: "Death Note",
           completed: false,
+          removed: true,
           discription:
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deseruntquisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!",
         },
@@ -54,11 +71,13 @@ const state = {
           id: 2,
           label: "Fairy Tale",
           completed: true,
+          removed: true,
           discription: "3rd season is out!",
         },
         {
           id: 3,
           label: "Naruto",
+          removed: false,
           completed: false,
           discription: "Let`s just try...",
         },
@@ -66,24 +85,29 @@ const state = {
     },
     {
       label: "Movies",
+      removed: false,
+      fullRemoved: false,
       list: [
         {
           id: 1,
           label: "Black horse",
           completed: true,
+          removed: false,
           discription:
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deseruntquisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!",
+            "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
         },
         {
           id: 2,
           label: "Black",
           completed: false,
+          removed: false,
           discription: "3rd season is out!",
         },
         {
           id: 3,
           label: "Green book",
           completed: false,
+          removed: false,
           discription: "Let`s just try...",
         },
       ],
@@ -93,6 +117,31 @@ const state = {
 };
 
 const getters = {
+  getRemovedType: (state) => {
+    const type = [];
+    state.todos.forEach((todoType) => {
+      if (todoType.removed === true) {
+        type.push(todoType);
+      }
+    });
+
+    return type;
+  },
+
+  getRemovedTodos: (state) => {
+    let todoList = [];
+    state.todos.forEach((todoType) => {
+      if (todoType.label === state.currentType) {
+        todoType.list.forEach((todo) => {
+          if (todo.removed === true) {
+            todoList.push(todo);
+          }
+        });
+      }
+    });
+    return todoList;
+  },
+
   getTodosType: (state) => {
     return state.todos;
   },
@@ -165,6 +214,7 @@ const mutations = {
       state.todos.forEach((todoType) => {
         if (todoType.label === state.currentType) {
           todo.id = todoType.list.length;
+          todo.removed = false;
           todoType.list.push(todo);
         }
       });
